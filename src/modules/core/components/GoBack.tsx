0e86a32ffe0e6d16bb971e8359fc4ebd992466e7
Gilbert Acosta <155097224+gajeeldev@users.noreply.github.com>
2024-06-30 19:46:38 -0400

    Update GoBack.tsx

--- src/modules/core/components/GoBack.tsx
+++ src/modules/core/components/GoBack.tsx
@@ -1,21 +1,27 @@
 import { useRouter } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
-import { Platform, Text, TouchableOpacity, View } from 'react-native';
+import { Platform, TouchableOpacity, View } from 'react-native';
 
 export const GoBack = () => {
   const router = useRouter();
 
   return (
     <TouchableOpacity onPress={() => router.back()}>
-      <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: 'rgba(245, 245, 245, 0.15)',
+          borderRadius: 24,
+          padding: 3,
+        }}>
         <Ionicons
           name={Platform.OS === 'ios' ? 'chevron-back-outline' : 'arrow-back'}
           size={26}
           color="white"
+          style={{ left: Platform.OS === 'ios' ? -2 : 0 }}
         />
-        <Text style={{ color: 'white', fontSize: 18 }}>
-          {Platform.OS === 'ios' ? 'Back' : null}
-        </Text>
       </View>
     </TouchableOpacity>
   );
